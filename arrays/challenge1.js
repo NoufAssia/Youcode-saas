baa1b70d8db8c200efae585d05ceb478992d2af6
NoufAssia <assiahamdan2021@gmail.com>
2026-09-19 05:29:05 +0100

    first array ex

---
+++ arrays/challenge1.js
@@ -0,0 +1,21 @@
+let array = [1, 9, 0, 5, 6, 7];
+
+function length(array)
+{
+	let i = 0;
+	while (i in array)
+	{
+		i++;
+	}
+	return (i);
+}
+
+function affiche(array)
+{
+	for (let i = 0; i < length(array); i++)
+	{
+		console.log(array[i]);
+	}
+}
+
+affiche(array);
